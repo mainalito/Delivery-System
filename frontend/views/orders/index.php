7603573dcd221e252a0aca604c0c5a347aefe1e3
Charles Maina <charles.maina@attain-es.com>
2023-11-02 17:30:37 +0300

    ui side orders frontend

--- frontend/views/orders/index.php
+++ frontend/views/orders/index.php
@@ -14,6 +14,25 @@ $this->title = 'Orders';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="orders-index">
+    <div class="card mb-3"> <!-- Adjust the max-width as needed -->
+        <div class="row no-gutters">
+
+            <!-- Image Column -->
+            <div class="col-md-4">
+                <img src="..." alt="Card image cap" class="img-fluid rounded-start"> <!-- Use "..." for the path to your image -->
+            </div>
+
+            <!-- Content Column -->
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
     <h1><?= Html::encode($this->title) ?></h1>
 
